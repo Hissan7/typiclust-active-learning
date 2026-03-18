3dd64a5641103bac0474304d88fa47c2c2fb4e30
k23136072 <hissan.omar@kcl.ac.uk>
2026-03-18 16:38:26 +0000

    add centrality helper function

--- src/typicality.py
+++ src/typicality.py
@@ -7,6 +7,7 @@ def compute_typicality_of_points(cluster_features: np.ndarray, k: int = 20) -> n
     Computes typicality for each point in a cluster
     typicality = 1 / average distance to k nearest neighbours
     """
+
     if len(cluster_features) == 1:
         return np.array([float("inf")])
 
@@ -17,12 +18,24 @@ def compute_typicality_of_points(cluster_features: np.ndarray, k: int = 20) -> n
 
     distances, _ = nbrs.kneighbors(cluster_features)
 
-    # remove self-distance at index 0
     neighbour_distances = distances[:, 1:]
 
     avg_distances = neighbour_distances.mean(axis=1)
-    
-    #typical equation 
+
     typicality = 1.0 / (avg_distances + 1e-8)
 
     return typicality
+
+
+def compute_centrality(cluster_features: np.ndarray) -> np.ndarray:
+    """
+    Computes centrality = inverse distance to cluster centroid
+    """
+
+    centroid = np.mean(cluster_features, axis=0)
+
+    distances = np.linalg.norm(cluster_features - centroid, axis=1)
+
+    centrality = 1.0 / (distances + 1e-8)
+
+    return centrality
